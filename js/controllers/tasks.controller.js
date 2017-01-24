8ba3b76f2c328e088229b03d2188fb1e8f6e46d8
github username <github email address>
2017-01-24 18:05:07 -0500

    Done.

--- js/controllers/tasks.controller.js
+++ js/controllers/tasks.controller.js
@@ -1,3 +1,45 @@
 'use strict';
 // Tasks Controller
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var TasksController = function () {
+	function TasksController() {
+		_classCallCheck(this, TasksController);
+
+		this.$addTaskForm = $('#add_task');
+		this.$taskDescriptionInput = $('#task_description');
+		this.$selectListMenu = $('#select_list');
+		this.$taskPriorityInput = $('#task_priority');
+		this.$wrapper = $('#wrapper');
+	}
+
+	_createClass(TasksController, [{
+		key: 'init',
+		value: function init() {
+			var _this = this;
+
+			this.$addTaskForm.submit(function (event) {
+				event.preventDefault();
+				for (var i = 0; i < List.all.length; i++) {
+					if (List.all[i].id == _this.$selectListMenu.val()) {
+						new Task(_this.$taskDescriptionInput.val(), _this.$taskPriorityInput.val(), List.all[i]).build();
+					}
+				}
+				$('.destroy-task').click(function () {
+					for (var _i = 0; _i < List.all.length; _i++) {
+						debugger;
+						if (List.all[_i].id == this.parentElement.getAttribute('data-id')) {
+							List.all[_i].tasks[this.parentElement.getAttribute('data-id')] = null;
+						}
+					}
+					this.parentElement.remove();
+				});
+			});
+		}
+	}]);
+
+	return TasksController;
+}();
--- js/models/list.js
+++ js/models/list.js
@@ -1,3 +1,39 @@
 'use strict';
 // List Model
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var List = function () {
+	function List(title) {
+		_classCallCheck(this, List);
+
+		this.title = title;
+		this.id = List.all.length;
+		this.tasks = [];
+		List.all.push(this);
+	}
+
+	_createClass(List, [{
+		key: 'listEl',
+		value: function listEl() {
+			return '<div class="list"><h2><button class="destroy-list">x</button> ' + this.title + '</h2><ul id="list-' + this.id + '" data-id="' + this.id + '"></ul></div>';
+		}
+	}, {
+		key: 'optionEl',
+		value: function optionEl() {
+			return '<option value="' + this.id + '">' + this.title + '</option>';
+		}
+	}, {
+		key: 'build',
+		value: function build() {
+			$('#select_list').append(this.optionEl());
+			$('#lists').append(this.listEl());
+		}
+	}]);
+
+	return List;
+}();
+
+List.all = [];
